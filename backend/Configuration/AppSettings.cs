586638c37024975bc9c45cf0c8338b8737be2533
atheeralmusallam-creator <atheeralsttam@gmail.com>
2026-05-18 19:24:00 +0300

    Delete backend directory

--- backend/Configuration/AppSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Fabric.API.Configuration;
-
-public class AppSettings
-{
-    public string OpenAIApiKey { get; set; } = string.Empty;
-    public string AnthropicApiKey { get; set; } = string.Empty;
-    public double DefaultConfidenceThreshold { get; set; } = 0.75;
-    public string AllowedOrigins { get; set; } = "http://localhost:4200";
-}
